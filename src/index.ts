be9b628fb22e6b230551474404d96e7123f325d6
Artem B <artem.basa@gmail.com>
2024-09-17 17:43:33 +0200

    Added ETH gas price

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 /*global fetch*/
+import { Web3 } from 'web3';
 
 export const handler = async (event) => {
 
@@ -9,7 +10,16 @@ export const handler = async (event) => {
     const chatId = '258432167';
 
     // Message you want to send
-    const message = 'Hello, this is a message from my bot!';
+    // const message = 'Hello, this is a message from my bot!';
+
+    // Fetch chain ID and gas price
+    const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/0q-QzDv9lW42FgxhtxCxdyaOdRgccGiw');
+    const chainId = await web3.eth.getChainId();
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasPriceGwei = web3.utils.fromWei(gasPrice, 'gwei');
+    // Prepare the message
+    const message = `Chain ID: ${chainId}, Gas Price (Gwei): ${parseFloat(gasPriceGwei).toFixed(2)}`;
+
     const url = `https://api.telegram.org/bot${token}/sendMessage`;
     const params = {
         chat_id: chatId,
